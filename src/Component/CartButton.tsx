e0c492368ca55fbc0024c59992739c135481df97
Michael Zabka <zabka.michael@gmail.com>
2019-05-08 21:48:04 +0200

    Add popup for shopping cart

--- src/Component/CartButton.tsx
+++ src/Component/CartButton.tsx
@@ -6,12 +6,32 @@ import './CartButton.css';
 type IProps = { children?: ReactNode } & ICartValue;
 
 const CartButton: React.FC<IProps> = (props: IProps) => {
-	return (
+	return <div className="shoppingCart">
 		<i
-			className={classNames("shoppingCart fa fa-shopping-cart", {
+			className={classNames("fa fa-shopping-cart", {
 				'active': props.items.length > 0,
 			})}
 		/>
-	);
+		{props.items.length > 0 && window.location.pathname !== '/kosik' ? (
+			<div
+				className="popover fade show bs-popover-bottom"
+			>
+				<div className="arrow"></div>
+				<h3 className="popover-header"></h3>
+				<div className="popover-body">
+					Právě jste si přidali nezapomenutelný zážitek do vašeho košíku.<br/>
+					Pro dokončení pokračujte do objednávky.
+					<a
+						href="/kosik"
+						className="btn btn-primary"
+					>
+						<i className="fa fa-shopping-cart"/>
+						<br/>
+						Dokončit objednávku
+					</a>
+				</div>
+			</div>
+		) : null}
+	</div>;
 }
 export default withCart(CartButton);

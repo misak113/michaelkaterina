34980c0eb6f503d42fe539efd3ffd2ae54ecb400
Michael Zabka <zabka.michael@gmail.com>
2019-08-25 12:21:16 +0200

    Update gifts page

--- src/Pages/GiftsPage.tsx
+++ src/Pages/GiftsPage.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import classNames from 'classnames';
 import './GiftsPage.css';
+import giftsImage from './gifts.jpg';
 
 const GiftsPage: React.FC = () => {
+	const giftsImageElement = (className: string) => (
+		<img src={giftsImage} className={classNames("giftsIimage", className)} alt="Svatební dary"/>
+	);
 	return <div className="gifts">
-		<h2>Nad touto součástí našeho dne stále přemýšlíme, ale už brzy dáme něco dohromady 😊</h2>
+		{giftsImageElement('before')}
+		<h2>Svatební dary</h2>
+		<p>
+			Jelikož už spolu nějaký čas obýváme jednu domácnost, máme vše z většiny vybavené.
+			Proto bychom vás raději poprosili o obálku. A kdybyste nám do ní i něco malého dali,
+			o to větší radost bychom měli 😊
+		</p>
+		{giftsImageElement('after')}
 	</div>;
 };
 export default GiftsPage;

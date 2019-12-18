a34d109820799c5d82f7fc98b68e83d8daca70ad
Michael Zabka <zabka.michael@gmail.com>
2019-12-19 00:24:17 +0100

    Sort files by creation date

--- tools/extract-images-filenames.js
+++ tools/extract-images-filenames.js
@@ -1,6 +1,21 @@
 
 const fs = require('fs');
+const { execSync } = require('child_process');
 
-const filenames = fs.readdirSync(__dirname + '/../photos');
+const imagesPath = __dirname + '/../photos';
 
-fs.writeFileSync(__dirname + '/../src/Pages/imagesFileNames.json', JSON.stringify(filenames));
+const filenames = fs.readdirSync(imagesPath);
+const files = [];
+
+for (const filename of filenames) {
+    const filePath = imagesPath + '/' + filename;
+    const command = `identify -verbose ${filePath}  | grep "exif:DateTimeDigitized:" | awk -F ": " '{print $2}'`;
+    const res = execSync(command);
+    const takenAt = new Date(res.toString().replace(':', '-').replace(':', '-'));
+    files.push({ filename, takenAt });
+    console.log(filename, takenAt);
+}
+
+const sortedFiles = files.sort((a, b) => a.takenAt.valueOf() - b.takenAt.valueOf());
+
+fs.writeFileSync(__dirname + '/../src/Pages/imagesFileNames.json', JSON.stringify(sortedFiles.map((file) => file.filename)));
